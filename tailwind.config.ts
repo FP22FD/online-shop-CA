3975a822ec02fb92e950ed437e1b5f62374f382c
FP22FD <112474910+FP22FD@users.noreply.github.com>
2024-09-06 23:15:50 +0200

    Implement a default theme customization

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,36 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'main': '#0D6EFD',
+      'darker': '#2659BF',
+      'lighter': '#99BBFF',
+      'subtitle': '#E3EDFF',
+      'primary': '#0D6EFD',
+      'background-primary': '#F2FDFF',
+      'background-secondary': '#101935',
+      'background-default': '#FFFFFF',
+      'typo-default-dark': '#212427',
+      'typo-default-light': '#FCFCFC',
+      'typo-secondary-light': '#ABB5BE',
+      'typo-secondary-dark': '#54595E',
+      'primary-grey-light': '#E9ECEF',
+      'secondary-grey-light': '#E9ECEF',
+      'status-success': '#28A745',
+      'status-error': '#DC3545',
+    },
+    fontFamily: {
+      sans: ['Sen', 'sans-serif'],
+      serif: ['Zilla Slab', 'serif'],
+    }
+    },
   },
   plugins: [],
 }
