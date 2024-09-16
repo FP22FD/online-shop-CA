bd002cebcb3276130685009b123159c0e86f8217
FP22FD <112474910+FP22FD@users.noreply.github.com>
2024-09-16 15:15:08 +0200

    Vitest setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+///<reference types="vitest" />
+///<reference types="vite/client" />
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./vitest.setup.ts'],
+    // setupFiles: ['.src/vitest.setup.ts'],
+    // testTimeout: 5000,
+    // reporters: ['verbose']
+  },
+});
