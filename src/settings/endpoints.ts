b495911701a52e70a54f1006117c379ee67c5547
FP22FD <112474910+FP22FD@users.noreply.github.com>
2024-10-02 12:33:45 +0200

    Setup API endpoints

---
+++ src/settings/endpoints.ts
@@ -0,0 +1,4 @@
+//storage API endpoints
+
+export const API_PRODUCTS = 'https://v2.api.noroff.dev/online-shop';
+export const API_PRODUCT = (id: string) => `https://v2.api.noroff.dev/online-shop/${id}`;
