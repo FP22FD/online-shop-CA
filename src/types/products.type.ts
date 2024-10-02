966e7dc7dc67832499b62e179d2480c87294f2d8
FP22FD <112474910+FP22FD@users.noreply.github.com>
2024-10-02 12:50:28 +0200

    Update storage types file

--- src/types/products.type.ts
+++ src/types/products.type.ts
@@ -81,3 +81,14 @@ export interface Meta {
   pageCount: number;
   totalCount: number;
 }
+
+// -------product to cart context----------------
+export interface CartProduct {
+  id: string;
+  title: string;
+  price: number;
+  discountedPrice: number;
+  image: string;
+  imageAlt: string;
+  quantity: number;
+}
