4f05b528b425dde6edd4fc12c22c6e8ddf56727e
FP22FD <112474910+FP22FD@users.noreply.github.com>
2024-10-02 12:47:50 +0200

    Refactoring products component

--- src/pages/home/Products.tsx
+++ src/pages/home/Products.tsx
@@ -1,50 +1,40 @@
 import { useEffect, useState } from 'react';
-import { FetchProducts } from '../../hooks/fetchProducts';
-import { Product } from '../../types/products.type';
+import { useFetchProducts } from '../../hooks/fetchProducts';
 import Card from './Card';
+import { Product } from '../../types/products.type';
+import SearchBar from './AutocompleteSearchBar';
+import Spinner from '../../components/shared/Spinner';
 
 const Products = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [errorMsg, setErrorMsg] = useState('');
-  const [products, setProducts] = useState<Product[]>([]);
+  const { data, error, loading } = useFetchProducts();
   const [filteredProducts, setFilteredProducts] = useState<Product[]>([]);
 
   useEffect(() => {
-    async function load() {
-      setIsLoading(true);
-      const [fetchError, fetchProductsData] = await FetchProducts();
-      setErrorMsg(fetchError);
-      setProducts(fetchProductsData ? fetchProductsData : []);
-      setFilteredProducts(fetchProductsData ? fetchProductsData : []);
-      setIsLoading(false);
-    }
-    load();
-  }, []);
-
-  const resetProducts = () => {
-    setFilteredProducts(products);
-  };
+    setFilteredProducts(data ? data : []);
+  }, [data]);
 
   return (
     <div className="max-w-screen-xl mx-auto p-5 sm:p-10 md:p-16">
+      <SearchBar />
       <div className="border-b border-secondary-dark mb-5 flex justify-between text-sm">
-        <button
-          onClick={resetProducts}
-          type="button"
-          className="text-dark flex items-center pb-2 pr-2 border-b-2 border-primary uppercase"
-        >
+        <button type="button" className="text-dark flex items-center pb-2 pr-2 border-b-2 border-primary uppercase">
           <h1 className="font-semibold inline-block">Products</h1>
         </button>
       </div>
 
-      {isLoading && <div>Loading...</div>}
-      {errorMsg && (
-        <div id="errorListings" className="error invalid-feedback text-center my-5 py-5 d-none">
-          {errorMsg}
-        </div>
-      )}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10">
+        {loading && (
+          <div className="col-span-1 sm:col-span-2 md:col-span-3 lg:col-span-4 flex items-center justify-center h-64">
+            <Spinner />
+          </div>
+        )}
 
-      <div className="grid grid-cols-1 justify-items-center sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10">
+        {error && (
+          <div role="alert" className="error invalid-feedback text-center my-5 py-5">
+            {error}
+          </div>
+        )}
+
         {filteredProducts.map((product) => (
           <Card key={product.id} data={product} />
         ))}
