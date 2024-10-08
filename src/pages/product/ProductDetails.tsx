363b90b1540ac3321d0b4104e2be201dd9797725
FP22FD <112474910+FP22FD@users.noreply.github.com>
2024-10-08 15:27:53 +0200

    Refactoring product details

--- src/pages/product/ProductDetails.tsx
+++ src/pages/product/ProductDetails.tsx
@@ -79,10 +79,10 @@ const ProductDetails = () => {
   return (
     <section className="flex-grow">
       <div className="container mx-auto mt-10 md:px-0">
-        <div className="flex items-center">
+        <div className="flex items-center mb-2">
           <Link
             to="/"
-            className="flex items-center px-3 py-2 text-xs rounded ring-[1px] ring-primary-light ring-inset hover:bg-primary text-primary-dark hover:text-neutral font-semibold transition-colors duration-200 mb-2"
+            className="flex items-center px-3 py-2 text-xs rounded ring-[1px] ring-primary-light ring-inset hover:bg-primary text-primary-dark hover:text-neutral font-semibold transition-colors duration-200"
             aria-label="Go back to store"
           >
             <IoArrowBackOutline aria-hidden="true" className="mr-2" />
@@ -97,23 +97,25 @@ const ProductDetails = () => {
         </div>
 
         <div className="shadow-md my-10 border">
-          <div className="flex flex-col md:flex-row gap-6 p-5">
-            <div className="w-full md:w-1/3 flex-shrink-0 relative">
+          <div className="flex flex-col sm:flex-row gap-6 p-4">
+            <div className="w-full sm:w-1/3 flex-shrink-0 relative">
               <img
-                className="object-cover w-full max-h-64"
+                className="object-cover w-full h-full"
                 src={data.image.url}
                 alt={data.image.alt || 'Product image'}
               />
               {discountExists && (
-                <div className="bg-accent-purple text-neutral-light text-lg font-semibold absolute p-6 right-1 top-1 w-10 h-10 rounded-full inline-flex items-center justify-center shrink-0 grow-0">
-                  {discountPercentage}%
+                <div className="absolute right-1 top-1 flex items-center">
+                  <div className="bg-accent-purple text-neutral-light font-semibold flex items-center justify-center flex-grow rounded-full text-lg p-6 md:p-8 md:text-2xl w-10 h-10 lg:w-6 lg:h-6">
+                    {discountPercentage}%
+                  </div>
                 </div>
               )}
             </div>
 
-            <div className="w-full md:w-2/3 flex flex-col justify-between p-4 border">
+            <div className="w-full md:w-2/3 flex flex-col justify-between p-2 border">
               <h2 className="mb-2 tracking-tight text-text font-semibold text-xl">{data.title}</h2>
-              <p className="line-clamp-2 whitespace-break-spaces mb-2">{data?.description}</p>
+              <p className="whitespace-break-spaces mb-2">{data?.description}</p>
 
               <div className="flex items-end space-x-1 md:gap-4 mb-2">
                 <div className="font-medium">
