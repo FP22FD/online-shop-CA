2895837f8252942994c87bcbb1650fe9bb172e5b
FP22FD <112474910+FP22FD@users.noreply.github.com>
2024-10-10 00:46:57 +0200

    Update Tailwind theme breakpoints

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,10 +4,11 @@ module.exports = {
   theme: {
     extend: {
       screens: {
-        sm: '600px',
-        md: '900px',
-        lg: '1200px',
-        xl: '1440px',
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        xxl: '1536px',
       },
       fontFamily: {
         sans: ['Inter', 'sans-serif'],
