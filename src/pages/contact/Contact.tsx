757e0253e6fef7f1490fd5743f1278183a08028d
FP22FD <112474910+FP22FD@users.noreply.github.com>
2024-10-09 12:18:59 +0200

    Refactor: update Tailwind styles in contact for improved layout

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -5,11 +5,11 @@ import Form from './Form';
 const Contact = () => {
   return (
     <section className="flex-grow">
-      <div className="container mx-auto mt-10 md:px-0">
-        <div className="flex items-center">
+      <div className="container mx-auto mt-10 md:px-8 lg:px-16 xl:px-24">
+        <div className="flex items-center mb-2">
           <Link
             to="/"
-            className="flex items-center px-3 py-2 text-xs rounded ring-[1px] ring-primary-light ring-inset hover:bg-primary text-primary-dark hover:text-neutral font-semibold transition-colors duration-200 mb-2"
+            className="flex items-center px-3 py-2 text-xs rounded ring-[1px] ring-primary-light ring-inset hover:bg-primary text-primary-dark hover:text-neutral font-semibold transition-colors duration-200"
             aria-label="Go back to store"
           >
             <IoArrowBackOutline aria-hidden="true" className="mr-2" />
@@ -17,7 +17,13 @@ const Contact = () => {
           </Link>
         </div>
 
-        <div className="my-10  flex justify-center">
+        <div className="border-b border-secondary-dark mb-5 flex justify-between text-sm my-10">
+          <button type="button" className="text-dark flex items-center pb-2 pr-2 border-b-2 border-primary uppercase">
+            <h1 className="font-semibold inline-block">Contact Us</h1>
+          </button>
+        </div>
+
+        <div className="my-10 flex justify-center">
           <div className="flex flex-col md:flex-row gap-6 p-3 md:divide-x w-fit border shadow-md">
             <div className="flex flex-col py-6 space-y-6">
               <div className="md:px-6">
