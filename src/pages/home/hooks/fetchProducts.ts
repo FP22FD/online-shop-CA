e30f10e0aaf67d38d5a12b99dac5899c5b6e0d35
FP22FD <112474910+FP22FD@users.noreply.github.com>
2024-10-10 00:39:48 +0200

    Implement controller signal cleanup hooks to custom fetch hooks

---
+++ src/pages/home/hooks/fetchProducts.ts
@@ -0,0 +1,54 @@
+import { useState, useEffect } from 'react';
+import { ErrorHandler } from '../../../shared/utils/errorHandler';
+import { AllProductsResponse, Product } from '../../../types/products.type';
+import { API_PRODUCTS } from '../../../shared/utils/endpoints';
+
+export function useFetchProducts() {
+  const [data, setData] = useState<Product[] | null>(null);
+  const [error, setError] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const { signal } = controller;
+
+    const fechtData = async () => {
+      try {
+        const response = await fetch(API_PRODUCTS, { signal });
+
+        if (response.ok) {
+          const productsData: AllProductsResponse = await response.json();
+
+          if (!controller.signal.aborted) {
+            const data = productsData.data;
+            setData(data);
+            console.log(data);
+
+            setError('');
+          }
+        } else {
+          const eh = new ErrorHandler(response);
+          const msg = await eh.getErrorMessage();
+          setError(msg);
+          setData(null);
+        }
+      } catch (e) {
+        if (e instanceof Error && e.name !== 'AbortError') {
+          setError('Could not show the products!');
+          console.log(e);
+          setData(null);
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fechtData();
+
+    return () => {
+      controller.abort();
+    };
+  }, []);
+
+  return { data, loading, error };
+}
--- src/pages/product/hooks/fetchProduct.ts
+++ src/pages/product/hooks/fetchProduct.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
-import { ErrorHandler } from '../components/shared/errorHandler';
-import { API_PRODUCT } from '../settings/endpoints';
-import { ProductResponse, SingleProduct } from '../types/products.type';
+import { ErrorHandler } from '../../../shared/utils/errorHandler';
+import { API_PRODUCT } from '../../../shared/utils/endpoints';
+import { ProductResponse, SingleProduct } from '../../../types/product.type';
 
 export function useFetchProduct(id: string): { data: SingleProduct | null; loading: boolean; error: string } {
   const [data, setData] = useState<SingleProduct | null>(null);
@@ -9,9 +9,12 @@ export function useFetchProduct(id: string): { data: SingleProduct | null; loadi
   const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
+    const controller = new AbortController();
+    const { signal } = controller;
+
     const fechtData = async () => {
       try {
-        const response = await fetch(API_PRODUCT(id));
+        const response = await fetch(API_PRODUCT(id), { signal });
 
         if (response.ok) {
           const productData: ProductResponse = await response.json();
@@ -25,16 +28,22 @@ export function useFetchProduct(id: string): { data: SingleProduct | null; loadi
           setError(msg);
           setData(null);
         }
-      } catch (error) {
-        setError('Could not show the product!');
-        console.log(error);
-        setData(null);
+      } catch (e) {
+        if (e instanceof Error && e.name !== 'AbortError') {
+          setError('Could not show the product!');
+          console.log(e);
+          setData(null);
+        }
       } finally {
         setLoading(false);
       }
     };
 
     fechtData();
+
+    return () => {
+      controller.abort();
+    };
   }, [id]);
 
   return { data, loading, error };
