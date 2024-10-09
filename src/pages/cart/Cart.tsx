5f869512a38af2e3e9a0159f87afac7b2bcefb77
FP22FD <112474910+FP22FD@users.noreply.github.com>
2024-10-10 00:54:19 +0200

    Refactor: adjust Tailwind styles in Cart component

--- src/pages/cart/Cart.tsx
+++ src/pages/cart/Cart.tsx
@@ -1,12 +1,9 @@
-import { useState } from 'react';
 import { IoArrowBackOutline } from 'react-icons/io5';
 import { Link } from 'react-router-dom';
-import Summary from './CartTotal';
-import CartProduct from './CartProduct';
+import Summary from './components/CartTotal';
+import CartProduct from './components/CartProduct';
 
 function Items() {
-  const [feedbackMessage] = useState<string | null>(null);
-
   return (
     <section className="flex-grow">
       <div className="container mx-auto mt-10 px-4 md:px-8 lg:px-16 xl:px-24">
@@ -21,19 +18,15 @@ function Items() {
           </Link>
         </div>
 
-        <div className="border-b border-secondary-dark mb-5 flex justify-between text-sm my-10">
+        <div className="border-b border-secondary-dark mb-10 flex justify-between text-sm my-10">
           <button type="button" className="text-dark flex items-center pb-2 pr-2 border-b-2 border-primary uppercase">
             <h1 className="font-semibold inline-block">Your cart</h1>
           </button>
         </div>
 
-        {feedbackMessage && <p className="text-success text-center">{feedbackMessage}</p>}
-
         <div className="flex flex-col md:flex-row justify-between md:gap-8">
-          <div className="shadow-md mt-10 border max-w-full md:max-w-3xl w-full md:w-4/5 min-h-[300px]">
-            <div>
-              <CartProduct />
-            </div>
+          <div className="relative shadow-md mt-4 border max-w-full md:max-w-3xl w-full md:w-4/5 md:h-fit">
+            <CartProduct />
           </div>
           <Summary />
         </div>
