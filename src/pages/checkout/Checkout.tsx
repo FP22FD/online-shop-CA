f1c5fb1acfda51a6507b67c3fe9f63413161c77e
FP22FD <112474910+FP22FD@users.noreply.github.com>
2024-10-10 00:56:52 +0200

    Refactor: adjust Tailwind styles in Checkout component for improved visibility

--- src/pages/checkout/Checkout.tsx
+++ src/pages/checkout/Checkout.tsx
@@ -9,7 +9,7 @@ const Checkout = () => {
           <IoCheckmarkCircleOutline />
         </div>
         <div className="text-center mt-6">
-          <h1 className="text-2xl text-black">Your order was Successful!</h1>
+          <h1 className="text-lg lg:text-2xl text-black">Your order was Successful!</h1>
           <p>Thank your purchase</p>
         </div>
         <div className="flex justify-center mt-6">
@@ -18,7 +18,7 @@ const Checkout = () => {
               <Link
                 type="button"
                 to="/"
-                className="flex items-center px-3 py-2 text-base rounded ring-[1px] bg-primary-light ring-primary-light ring-inset hover:bg-primary text-primary-dark hover:text-neutral-light font-bold transition-colors duration-200 mb-2"
+                className="flex items-center px-3 py-4  text-sm lg:text-base rounded ring-[1px] bg-primary-light ring-primary-light ring-inset hover:bg-primary text-primary-dark hover:text-neutral-light font-bold transition-colors duration-200 mb-2"
               >
                 <IoArrowBackOutline aria-hidden="true" className="mr-2" />
                 Go back to store
