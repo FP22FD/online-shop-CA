7f02f3c8e15ff884caefd02647e3615b81a23dce
FP22FD <112474910+FP22FD@users.noreply.github.com>
2024-10-09 12:17:32 +0200

    Refactor: update Tailwind styles for improved layout

--- src/pages/home/Products.tsx
+++ src/pages/home/Products.tsx
@@ -22,7 +22,7 @@ const Products = () => {
         </button>
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10">
         {loading && (
           <div className="col-span-1 sm:col-span-2 md:col-span-3 lg:col-span-4 flex items-center justify-center h-64">
             <Spinner />
